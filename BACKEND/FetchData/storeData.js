c2c4fc62adfbf0bc38bb09d6d032e8c8ba6c849c
ankitjsr1211 <ankryadav1211@gmail.com>
2024-07-21 11:47:54 +0530

    Update storeData.js

--- BACKEND/FetchData/storeData.js
+++ BACKEND/FetchData/storeData.js
@@ -1,10 +1,13 @@
 import content from "../model/contentList.js";
+import { movieList, tvList } from "./fetchData.js";
 import connection from "../db/index.js";
 
 export const storeCollection = async () => {
   try {
     await connection;
-    const totalCollection = [];
+   const movies = await movieList();
+    const tv = await tvList();
+    const totalCollection = [...movies, ...tv];
     await content.create(totalCollection);
     console.log("Movies uploaded to db");
   } catch (err) {
