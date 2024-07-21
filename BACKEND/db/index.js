a688c173f56784faf613a46793c5a56128f29765
ankitjsr1211 <ankryadav1211@gmail.com>
2024-07-21 11:49:37 +0530

    Update index.js

--- BACKEND/db/index.js
+++ BACKEND/db/index.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 
 config()
-const connection = mongoose.connect( process.env.db , {
+const connection = mongoose.connect( "mongodb+srv://ankityadav:Dream11@project.rgjegji.mongodb.net/?retryWrites=true&w=majority&appName=projectls", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
